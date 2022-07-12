b81c92003b4670f723a6df134604fd7b28f80063
randomhydrosol <randomhydrosol@glassrom.org>
2022-07-12 13:00:13 +0530

    add placeholder code
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+     let argv: Vec<String> = env::args().collect();
+     let argc = argv.len();
+     if !(String::from("zip").eq_ignore_ascii_case(&argv[0]) || String::from("unzip").eq_ignore_ascii_case(&argv[0])) {
+         println!("invalid mode");
+     } else {
+         println!("running in {} mode", &argv[0]);
+     }
+     if argc < 2 {
+         println!("Zipshim. Copyright 2022 GlassROM and randomhydrosol. Licensed under WTFPL\nExpected at least one argument");
+             std::process::exit(1);
+     }
 }
